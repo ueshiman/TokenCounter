8bf0a168bc15329bb0c324eedc84e3eba5457a6e
tateno <tateno@fairus.jp>
2024-04-03 07:03:50 +0900

    text-davinci-003完了

---
+++ TokenCounter03/CountTokenTextDavinci003.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.DeepDev;
+
+namespace TokenCounter03
+{
+    public class CountTokenTextDavinci003
+    {
+        private ITokenizer _tokenizer;
+
+        public CountTokenTextDavinci003()
+        {
+            GetKTokenCount("text-davinci-003").Wait();
+        }
+
+        private async Task GetKTokenCount(string modelName)
+        {
+            _tokenizer = await TokenizerBuilder.CreateByModelNameAsync(modelName);
+        }
+
+        public int CountToken(string text)
+        {
+            return _tokenizer.Encode(text).Count();
+        }
+    }
+}
--- TokenCounter03/Program.cs
+++ TokenCounter03/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            if(args .Length == 0)
+            {
+                Console.WriteLine("トークンをカウントするファイルの指定が必要です。");
+                return;
+            }
+
+            var countTokenTextDavinci003 = new CountTokenTextDavinci003();
+            var text = System.IO.File.ReadAllText(args[0]);
+            var count = countTokenTextDavinci003.CountToken(text);
+            Console.WriteLine($"トークン数: {count}");
         }
     }
 }
